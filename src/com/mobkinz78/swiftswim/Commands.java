34b3dc790a49a4249cf02881d4f8e33d3f9b4d6a
BuildTools <markbacon78@gmail.com>
2017-04-02 10:33:02 -0400

    Added command for directly adding worlds to configuration.

--- src/com/mobkinz78/swiftswim/Commands.java
+++ src/com/mobkinz78/swiftswim/Commands.java
@@ -31,6 +31,9 @@ public class Commands implements CommandExecutor {
                     if (args.length == 0) {
                         sender.sendMessage(Core.prefix + "§cNo argument given! Accepted arguments: ");
                         sender.sendMessage(Core.prefix + "§9Enable §r§lor §r§9disable");
+                        if(player.hasPermission("swiftswim.addworld") || player.isOp()){
+                            sender.sendMessage(Core.prefix + "§9To add a world for enabling Swift Swim, use §b/swiftswim addworld <worldname>");
+                        }
                         return true;
                     }
                     if (!enabledWorlds.contains(w.getName())) {
@@ -71,9 +74,34 @@ public class Commands implements CommandExecutor {
                         }
                     }
                 }
-                if(!player.hasPermission("swiftswim.use")){
+                if(!player.hasPermission("swiftswim.use") && !player.isOp()){
                     player.sendMessage(Core.prefix + "§9Sorry, you are not able to use swift swim!");
                     player.sendMessage(Core.prefix + "§9Required permission: §cswiftswim.use");
+                    return true;
+                }
+                if(player.hasPermission("swiftswim.addworld") || player.isOp()){ /* /swiftswim addworld <worldname> */
+                    if(args[0].equalsIgnoreCase("addworld")){
+                        if(args.length < 2){
+                            player.sendMessage(Core.prefix + "§9Please provide a worldname.");
+                            return true;
+                        }
+                        if(Core.getInstance().getConfig().getStringList("enabled-worlds").contains(args[1])){
+                            player.sendMessage("§9That world already has swift swim enabled!");
+                            return true;
+                        }
+                        List<String> worlds = Core.getInstance().getConfig().getStringList("enabled-worlds");
+                        worlds.add(args[1]);
+                        Core.getInstance().getConfig().set("enabled-worlds", worlds);
+                        player.sendMessage(Core.prefix + "§9Successfully added " + args[1] + " to swift swim's enabled worlds!");
+                        Core.getInstance().saveConfig();
+                        Core.getInstance().reloadConfig();
+                        return true;
+                    }
+                    return true;
+                }
+                if(!player.hasPermission("swiftswim.addworld") && !player.isOp()){
+                    player.sendMessage(Core.prefix + "§cSorry, you don't have access to this command!");
+                    player.sendMessage(Core.prefix + "§bRequired permission: §bswiftswim.addworld");
                     return true;
                 }
             }
